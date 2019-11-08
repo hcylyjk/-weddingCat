99333952954b404f869be31b616f8612fed276e7
lvxiaohui <1010154018@qq.com>
2019-11-08 21:48:58 +0800

    更新

--- dist/js/l_invitations.js
+++ dist/js/l_invitations.js
@@ -9,7 +9,7 @@ l_block.onmouseout = function (){
     l_navToolMenu.style.display = 'none';
 }
 
-// 二维码加载
+// 下载APP二维码加载
 var l_bannerBtnQrimg = document.querySelector('.l_banner-btn-qrimg');
 var l_bannerBtnBg = document.querySelector('.l_banner-btn-bg');
 l_bannerBtnBg.onmouseover = function(){
@@ -18,6 +18,57 @@ l_bannerBtnBg.onmouseover = function(){
 l_bannerBtnBg.onmouseout = function(){
     l_bannerBtnQrimg.style.display = 'none';
 }
+
+// 模版层的二维码加载
+var l_itemQr = document.querySelectorAll('.l_item-qr');
+var l_itemQrBlack = document.querySelectorAll('.l_item1-qr-black');
+var l_itemList = document.querySelectorAll('.l_item-list')
+for(var i=0,len=l_itemList.length;i<len;i++){
+    l_itemList[i].index = i;
+    l_itemList[i].onmouseover =function (){
+        l_itemQr[this.index].style.display = 'block';
+        l_itemQrBlack[this.index].style.display= 'block';
+    }
+    l_itemList[i].onmouseout =function (){
+        l_itemQr[this.index].style.display = 'none';
+        l_itemQrBlack[this.index].style.display= 'none';
+    }
+}
+
+// $(document).ready(function () {
+//     //滚动动画
+//     var animateStartHeight = 300;
+//     var windowHeight = $(window).height();
+//     var animateDOMs = $(".block");
+//     var animateItems = animateDOMs.map(function (key, item) {
+//         return {elem: item, offsetTop: $(item).offset().top, outerHeight: $(item).outerHeight(), isAnimate: false};
+//     });
+
+//     $(window).scroll(function () {
+//         var scrollTop = document.body.scrollTop || document.documentElement.scrollTop;
+//         //判断各个元素是否符合触发动画的标准，是的话触发
+//         for (var i = 0; i < animateItems.length; i++) {
+//             if (!animateItems[i].isAnimate && windowHeight + scrollTop - animateItems[i].offsetTop > animateStartHeight && scrollTop - animateItems[i].offsetTop - animateItems[i].outerHeight < animateStartHeight) {
+//                 if($(animateItems[i].elem).find('.l_animate')){
+//                     $(animateItems[i].elem).find('.l_animate').addClass("l_active");
+//                 }
+//                 var $imgLazys = $(animateItems[i].elem).find('img.lazy');
+//                 if($(animateItems[i].elem).hasClass('lazy')){
+//                     if($(animateItems[i].elem).attr('data-url')){
+//                         $(animateItems[i].elem).css('background-image','url('+animateItems[i].elem.dataset.url+')').removeAttr('_url').removeClass('lazy');
+//                     }
+//                 }
+//                 $imgLazys.each(function(index,item){
+//                     if($(item).attr('_src')){
+//                         $(item).attr('src',$(item).attr('_src')).removeAttr('_src').removeClass('lazy');
+//                     }else if($(item).attr('_url')){
+//                         $(item).css('background-image','url('+item.dataset.url+')').removeAttr('_url').removeClass('lazy');
+//                     }
+//                 })
+//                 animateItems[i].isAnimate = true;
+//             }
+//         }
+//     });
 
 
 
