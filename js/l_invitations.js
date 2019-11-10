98d0a547b0bf048e78483e653f63bcdbd45605bf
lvxiaohui <1010154018@qq.com>
2019-11-11 00:08:49 +0800

    更新

--- js/l_invitations.js
+++ js/l_invitations.js
@@ -1,21 +1,21 @@
 // 更多工具加载
-var l_block =document.querySelector('.l_block');
+var l_block = document.querySelector('.l_block');
 var l_navToolMenu = document.querySelector('.l_nav-tool-menu');
 
-l_block.onmouseover = function (){
+l_block.onmouseover = function () {
     l_navToolMenu.style.display = 'block';
 }
-l_block.onmouseout = function (){
+l_block.onmouseout = function () {
     l_navToolMenu.style.display = 'none';
 }
 
 // 下载APP二维码加载
 var l_bannerBtnQrimg = document.querySelector('.l_banner-btn-qrimg');
 var l_bannerBtnBg = document.querySelector('.l_banner-btn-bg');
-l_bannerBtnBg.onmouseover = function(){
+l_bannerBtnBg.onmouseover = function () {
     l_bannerBtnQrimg.style.display = 'block';
 }
-l_bannerBtnBg.onmouseout = function(){
+l_bannerBtnBg.onmouseout = function () {
     l_bannerBtnQrimg.style.display = 'none';
 }
 
@@ -23,17 +23,41 @@ l_bannerBtnBg.onmouseout = function(){
 var l_itemQr = document.querySelectorAll('.l_item-qr');
 var l_itemQrBlack = document.querySelectorAll('.l_item1-qr-black');
 var l_itemList = document.querySelectorAll('.l_item-list')
-for(var i=0,len=l_itemList.length;i<len;i++){
+for (var i = 0, len = l_itemList.length; i < len; i++) {
     l_itemList[i].index = i;
-    l_itemList[i].onmouseover =function (){
+    l_itemList[i].onmouseover = function () {
         l_itemQr[this.index].style.display = 'block';
-        l_itemQrBlack[this.index].style.display= 'block';
+        l_itemQrBlack[this.index].style.display = 'block';
     }
-    l_itemList[i].onmouseout =function (){
+    l_itemList[i].onmouseout = function () {
         l_itemQr[this.index].style.display = 'none';
-        l_itemQrBlack[this.index].style.display= 'none';
+        l_itemQrBlack[this.index].style.display = 'none';
     }
 }
+
+// 点击制作
+var l_qrWin = document.querySelector('.l_qr-win');
+var l_download = document.querySelector('.l_banner-btn-download')
+var l_close = document.querySelector('.l_background-download-close');
+var l_btn = document.querySelector('#l_btn');
+
+var oL_btn = l_btn.getAttribute('id')
+console.log(oL_btn)
+// l_download.onclick = function (e) {
+//     var ev = e || window.event;
+//     l_qrWin.style.display = 'block';
+// };
+// l_close.onclick = function () {
+//     l_qrWin.style.display = 'none';
+// };
+// clearTimeout(item);
+// var item = setTimeout(function () {
+//         document.onclick = function () {
+//             l_qrWin.style.display = 'none';
+//         }
+// }, 2000);
+
+
 
 // $(document).ready(function () {
 //     //滚动动画
