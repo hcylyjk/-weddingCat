684cf3de5491833fd2b9eb4fc5e7226696d0567d
ljj978649470 <978649470@qq.com>
2019-11-12 22:03:25 +0800

    1111

---
+++ dist/js/j_journey_list.js
@@ -0,0 +1,54 @@
+// 图片展示
+// var owrke = document.querySelectorAll('.j_work_center');
+// console.log(owrke)
+// var oload = document.querySelector('.j_load');
+
+
+// oload.onclick = function () {
+//     for (var i = 0; i < 10; i++) {
+
+        
+//         oload.style.display = 'none';
+//     }
+
+// }
+//三亚旅拍照
+var uls3 = document.querySelector('.j_list_warp ul');
+var oR3 = document.querySelector('.nave_right3');
+var oL3 = document.querySelector('.nave_left3');
+oR3.onclick = function () {
+    uls3.style.left = -1200 + 'px';
+    oL3.style.display = 'block';
+}
+oL3.onclick = function () {
+    uls3.style.left = 0 + 'px';
+    oL3.style.display = 'none';
+};
+
+// $(function(){
+//  console.log( $(window) .scroll(function() {}))  
+      
+    
+// })
+// console.log($(document).scrollTop())
+var fixnav = document.querySelector('.j_fiednnav')
+window.onscroll = function () {
+    var html = document.documentElement || document.body;
+    if(html<=480){
+        fixnav.style.position='fixed';
+        fixnav.style.top=0;
+        fixnav.style.right=0;
+    }
+    console.log(html.scrollTop);
+  }
+//   window.onscroll = function () {
+//     var scrollTop = document.documentElement.scrollTop || document.body.scrollTop; // 获取滚动高度
+//     // console.log(scrollTop, topContent.offsetHeight);
+//     if (scrollTop >= ) { // 滚动高度 > 头部内容高度
+//       right.style.position = 'fixed';
+//       right.style.top = 0;
+//       right.style.right = 0;
+//     } else {
+//       right.style.position = 'initial';
+//     }
+//   }
--- js/j_journey.js
+++ js/j_journey.js
@@ -1,6 +1,32 @@
 
 
- //城市客照
-$('.j_cityBlist ul ').mouseover=function(e){
-	
+//城市客照
+
+var uls = document.querySelector('.j_cityBlist ul');
+var oR = document.querySelector('.nave_right');
+var oL = document.querySelector('.nave_left');
+oR.onclick = function () {
+    uls.style.left = -1200 + 'px';
+    oL.style.display = 'block';
 }
+oL.onclick = function () {
+    uls.style.left = 0 + 'px';
+    oL.style.display = 'none';
+}
+
+//每月客照
+var uls1 = document.querySelector('.j_month_list ul');
+var oR1 = document.querySelector('.nave_right1');
+var oL1 = document.querySelector('.nave_left1');
+oR1.onclick = function () {
+    uls1.style.left = -1200 + 'px';
+    oL1.style.display = 'block';
+}
+oL1.onclick = function () {
+    uls1.style.left = 0 + 'px';
+    oL1.style.display = 'none';
+}
+
+//摄影师
+var oA = document.querySelectorAll('.j_teamr a')
+var oSpan = document.querySelectorAll('.j_teamr a span');
