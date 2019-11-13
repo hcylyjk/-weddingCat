d6db7894f28b1ecb9f67088100192985584f5318
ljj978649470 <978649470@qq.com>
2019-11-13 15:15:22 +0800

    1111

--- dist/js/j_journey_list.js
+++ dist/js/j_journey_list.js
@@ -1,17 +1,4 @@
-// 图片展示
-// var owrke = document.querySelectorAll('.j_work_center');
-// console.log(owrke)
-// var oload = document.querySelector('.j_load');
 
-
-// oload.onclick = function () {
-//     for (var i = 0; i < 10; i++) {
-
-        
-//         oload.style.display = 'none';
-//     }
-
-// }
 //三亚旅拍照
 var uls3 = document.querySelector('.j_list_warp ul');
 var oR3 = document.querySelector('.nave_right3');
@@ -53,45 +40,5 @@ oL3.onclick = function () {
 //     }
 //   }
 
-var uls4 = document.querySelector('.j_list_warps ul');
-console.log(uls4)
-var oR4 = document.querySelector('.nave_right4');
-var oL4 = document.querySelector('.nave_left4');
-var a = 0
-
-oR4.onclick = function () {
-   
-    a-=880
-   
-    uls4.style.left= a + 'px';
-    if(a==-8800){
-        oR4.onclick='';
-     }
-    oL4.style.display = 'block';
-   
-   
-}
-oL4.onclick = function () {
-   
-    a+=880
-    uls4.style.left = a + 'px';
-    if(a==0){
-        oR4.onclick='';
-        oL4.style.display = 'none';
-     }else{
-        oR4.onclick = function () {
-   
-            a-=880
-           
-            uls4.style.left= a + 'px';
-            if(a==-8800){
-                oR4.onclick='';
-             }
-            oL4.style.display = 'block';
-           
-           
-        }
-     }
-   
-};
+// 
 
