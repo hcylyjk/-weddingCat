89aeb03d943db5c48e5ec3dabdda28005052f39e
lvxiaohui <1010154018@qq.com>
2019-11-13 09:20:04 +0800

    更新

--- dist/js/l_Budget.js
+++ dist/js/l_Budget.js
@@ -3,10 +3,10 @@ var l_btn = document.querySelector('.l_header-btn');
 var l_wrap = document.querySelector('.l_customer-center-wrap');
 l_btn.onmouseover = function() {
     l_wrap.style.display = 'block';
-}
+};
 l_btn.onmouseout = function() {
     l_wrap.style.display = 'none';
-}
+};
 
 // 全部服务
 var l_nav = document.querySelector('.l_header-nav');
@@ -14,7 +14,18 @@ var l_title = document.querySelector('.l_header-nav-select')
 var l_list = document.querySelector('.l_header-select-list');
 l_title.onmouseover = function() {
     l_list.style.display = 'block';
-}
+};
 l_nav.onmouseout = function() {
     l_list.style.display = 'none';
-}
+};
+
+// 计算
+
+$('.l_budget_submit').click(function(){
+       var zys = $('#l_total_budget').val();
+       var zs = $('#l_table_count').val();
+       var hlZys = zys * 3/10
+       $('#l_totalParty').html(hlZys + '￥');
+       $('#l_price').html(hlZys/zs + '￥')
+    //    console.log( hlZys/zs);
+});
