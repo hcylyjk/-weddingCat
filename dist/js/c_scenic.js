ab4d7d17e04238712d49e9575870c123644d0d9c
Poiey <692300083@qq.com>
2019-11-13 10:39:24 +0800

    陈浩11.13

--- dist/js/c_scenic.js
+++ dist/js/c_scenic.js
@@ -2,7 +2,7 @@ $(function () {
     // 图片懒加载
     $(window).scroll(function () {
         for (var i = 0; i < $('.spotcontentcon').length; i++) {
-            if ($('.spotcontentcon').eq(i).offset().top <= ($(this).scrollTop() + $(this).height() - 200)) {
+            if ($('.spotcontentcon').eq(i).offset().top <= ($(this).scrollTop() + $(this).height())) {
                 var oSrc = $('.spotcontentcon').eq(i).find('.positionimg img').attr('data-src')
                 $('.spotcontentcon').eq(i).find('.positionimg img').attr('src', oSrc)
             }
